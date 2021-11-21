b3716adcd3224ee48c1290eb1816d176a792b019
christian <regaliasventes@gmail.com>
2021-11-21 14:26:15 -0500

    Primer controlador

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #get 'home/index'
   resources :widgets
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,7 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'welcome#index'
+  root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
