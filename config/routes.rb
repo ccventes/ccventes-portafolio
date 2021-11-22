df4f778239ca542f0368a73fd98a9ee6bcdd79c1
christian <regaliasventes@gmail.com>
2021-11-21 22:38:14 -0500

    incluyendo bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-
+  get 'home/about'
   root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
